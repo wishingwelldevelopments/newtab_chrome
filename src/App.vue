a3094742adb16f1ad9e59074298ff09f7620856b
19113 <swinb0@eq.edu.au>
2020-07-31 14:12:05 +1000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="quote">
+		<h1>{{ quote.quote }}</h1>
+		<p>{{ quote.name }}</p>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import json from './assets/quotes.json'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+	data(){
+		return{
+			quotes:json,
+			quote:[]
+		}
+	},
+	created(){
+		const i = Math.floor(Math.random() * this.quotes.length);
+		this.quote = this.quotes[i];
+	}
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+  font-family: 'quote';
+	src:  url('./assets/font.woff2') format('woff2');
+}
+*{
+	margin:0px;
+	padding:0px;
+}
+body{
+	background-color:#ececec;
+	display:flex;
+	width:100%;
+	height:100vh;
+	justify-content:center;
+	align-items:center;
+	flex-direction:column;
+	text-align:center;
+}
+p{
+	color:grey;
+	font-size:20px;
+	font-family: Cantarell, Arial, sans-serif;
+	text-transform:uppercase;
+	font-weight:bold;
+}
+h1{
+	width:460px;
+	color:#2b2b2b;
+	font-family: "quote", Arial, sans-serif;
+	margin-bottom:30px;
+	font-size:40px;
 }
 </style>
